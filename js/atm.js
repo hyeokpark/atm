8f9c9e0d9e97f72e7960135a83470b1841a151d4
hyeokpark92 <hyeokpark92@gmail.com>
2022-05-30 09:21:39 +0900

    commit

--- js/atm.js
+++ js/atm.js
@@ -2,7 +2,7 @@ var _Atm = (function () {
   var map = null;
 
   var baseMapUrl =
-    "https://map.pstatic.net/nrb/styles/basic/1653019652/{z}/{x}/{y}@2x.png?mt=bg.ol.sw.ar.lko";
+    "https://map.pstatic.net/nrb/styles/basic/1653629123/{z}/{x}/{y}@2x.png?mt=bg.ol.sw.ar.lko";
 
   var dataArr = [
     {
