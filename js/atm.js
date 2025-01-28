9415091701626af2a462d392b19bf35bd57f0308
hyeokpark <hyeokpark92@gmail.com>
2025-01-28 16:11:35 +0900

    Update atm.js

--- js/atm.js
+++ js/atm.js
@@ -2,7 +2,7 @@ var _Atm = (function () {
   var map = null;
 
   var baseMapUrl =
-    'https://map.pstatic.net/nrb/styles/basic/1735313972/{z}/{x}/{y}@2x.png?mt=bg.ol.sw.ar.lko';
+    'https://map.pstatic.net/nrb/styles/basic/1737686097/{z}/{x}/{y}@2x.png?mt=bg.ol.sw.ar.lko';
 
   var dataArr = [
     {
